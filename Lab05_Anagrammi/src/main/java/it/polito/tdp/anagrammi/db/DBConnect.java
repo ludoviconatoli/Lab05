3ab7e426b0fe90c14243849a9a07a1591d608605
ludov <ludov@LAPTOP-8O3J38D4>
2021-04-24 17:46:00 +0200

    fine

--- Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/db/DBConnect.java
+++ Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/db/DBConnect.java
@@ -4,10 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 public class DBConnect {
 
+	static private HikariDataSource ds = null;
 	public static Connection getConnection() throws SQLException {
 		String jdbcURL = "jdbc:mysql://localhost/dizionario?user=root&password=1999";
-		return DriverManager.getConnection(jdbcURL);
+		
+		if(ds== null) {
+			ds = new HikariDataSource();
+			
+			ds.setJdbcUrl(jdbcURL);
+			//ds.setUsername("root");
+			//ds.setPassword("1999");
+			//già fatto nella url ma volendo lo posso fare qua in alternativa
+		}
+		return ds.getConnection(); //al posto di DriverManager.getConnection(jdbcURL)
 	}
 }
